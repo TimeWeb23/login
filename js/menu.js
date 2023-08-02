918501b9ddc67b52af2810a6e80714c4e7d5a304
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-01 22:10:08 -0300

    Inicializando

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+
+const botaoMobile = document.querySelector("#btn-mobile");
+
+botaoMobile.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    const navBar = document.querySelector("#nav");
+
+    navBar.classList.toggle('active');
+}
